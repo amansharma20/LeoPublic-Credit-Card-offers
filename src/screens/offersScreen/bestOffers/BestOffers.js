de7a3d4b39bf2a1214034afc98f16375b7844d2b
Aman <amanacts20@gmail.com>
2021-08-19 16:30:20 +0530

    offers screen small changes

--- src/screens/offersScreen/bestOffers/BestOffers.js
+++ src/screens/offersScreen/bestOffers/BestOffers.js
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-  },
+  }, 
   contentContainer: {
     paddingBottom: Responsive.height(20),
   },
