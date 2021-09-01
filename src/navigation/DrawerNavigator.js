e4e4a3042c528d27385bddf3514eab50d467cebb
Aman <amanacts20@gmail.com>
2021-09-01 14:36:02 +0530

    Side bar navigator and some ui enhancements

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,4 +1,5 @@
-
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import StackNavigator from './StackNavigator';
@@ -8,26 +9,37 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  Image
 } from 'react-native';
 import { Responsive } from '../utils/layouts/Layout';
+import { icons, SIZES } from '../constants';
 
+import About from '../screens/drawer/About';
+import TermsAndConditions from '../screens/drawer/TermsAndConditions';
+
+import Support from '../screens/drawer/Support';
+import FAQs from '../screens/drawer/FAQs';
+import { useNavigation } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
 const DrawerContent = ({ }) => {
-
+  const navigation = useNavigation();
   return (
-    <View style={{
-      width: '100%',
-      height: Responsive.screenHeight(),
-      backgroundColor: 'white',
-    }}>
+    <View
+      style={{
+        width: '100%',
+        height: Responsive.screenHeight(),
+        backgroundColor: 'white',
+      }}>
       <View style={styles.header}>
         {/* <FastImage source={require('../../assets/images/logo.gif')} resizeMode={FastImage.resizeMode.cover} style={{
                   width: '100%',
                   height: '100%',
               }}/> */}
       </View>
-      <ScrollView style={styles.userInformation} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        style={styles.userInformation}
+        showsVerticalScrollIndicator={false}>
         <TouchableOpacity activeOpacity={0.5} style={styles.item}>
           <View style={styles.leftIcon}>
             {/* <FastImage source={user.loggedIn ? require('../../assets/images/avatar/avatar1.png') : require('../../assets/images/useravatar.png')} resizeMode={FastImage.resizeMode.contain} style={{
@@ -38,20 +50,43 @@ const DrawerContent = ({ }) => {
           <View style={styles.content}>
             {/* <CommonText style={styles.text}>{user.loggedIn ? user.user.username : lang.guest}</CommonText> */}
           </View>
-          <View style={styles.rightIcon}>
-          </View>
+          <View style={styles.rightIcon} />
         </TouchableOpacity>
 
-        <TouchableOpacity activeOpacity={0.5} style={styles.item}>
-          <View style={styles.leftIcon}>
-            {/* <Mail/> */}
-          </View>
+        <TouchableOpacity onPress={() => navigation.navigate('About')} activeOpacity={0.5} style={styles.item}>
+          <View style={styles.leftIcon}>{/* <Mail/> */}</View>
           <View style={styles.content}>
-            <Text style={styles.text}>HELo </Text>
+            <Image source={icons.aboutIcon} style={styles.iconStyle} />
+            <Text style={styles.text}>About </Text>
           </View>
-          <View style={styles.rightIcon}>
+          <View style={styles.rightIcon} />
+        </TouchableOpacity>
 
+        <TouchableOpacity onPress={() => navigation.navigate('FAQs')} activeOpacity={0.5} style={styles.item}>
+          <View style={styles.leftIcon}>{/* <Mail/> */}</View>
+          <View style={styles.content}>
+          <Image source={icons.faqsIcon} style={styles.iconStyle} />
+            <Text style={styles.text}>FAQs</Text>
           </View>
+          <View style={styles.rightIcon} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('TermsAndConditions')} activeOpacity={0.5} style={styles.item}>
+          <View style={styles.leftIcon}>{/* <Mail/> */}</View>
+          <View style={styles.content}>
+          <Image source={icons.tndcIcon} style={styles.iconStyle} />
+            <Text style={styles.text}>T&C</Text>
+          </View>
+          <View style={styles.rightIcon} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Support')} activeOpacity={0.5} style={styles.item}>
+          <View style={styles.leftIcon}>{/* <Mail/> */}</View>
+          <View style={styles.content}>
+          <Image source={icons.supportIcon} style={styles.iconStyle} />
+            <Text style={styles.text}>Support</Text>
+          </View>
+          <View style={styles.rightIcon} />
         </TouchableOpacity>
         {/* <CommonLine style={{marginTop: Responsive.height(2)}}/>
               <CommonLine style={{marginTop: Responsive.height(2)}}/> */}
@@ -59,7 +94,6 @@ const DrawerContent = ({ }) => {
     </View>
   );
 };
-
 
 // export default function MyDrawer() {
 //   return (
@@ -144,11 +178,7 @@ const DrawerContent = ({ }) => {
 //   },
 // });
 
-
-
 export default function DrawerNavigator() {
-
-
   return (
     <Drawer.Navigator
       initialRouteName="Showing"
@@ -160,22 +190,58 @@ export default function DrawerNavigator() {
         activeBackgroundColor: 'transparent',
         activeTintColor: 'white',
         inactiveTintColor: 'white',
-        headerShown: false
-
+        headerShown: false,
       }}
       sceneContainerStyle={{ backgroundColor: 'transparent' }}
       drawerContent={props => {
         return <DrawerContent {...props} />;
-      }}
-    >
-      <Drawer.Screen name='MyCineScreens'>
+      }}>
+      <Drawer.Screen name="MyCineScreens">
         {props => <StackNavigator {...props} />}
       </Drawer.Screen>
+      <Drawer.Screen
+        options={{
+          title: 'About',
+          drawerIcon: () => (
+            <Image source={icons.aboutIcon} style={styles.iconStyle} />
+          ),
+        }}
+        name="About"
+        component={About}
+      />
+      <Drawer.Screen
+        options={{
+          title: 'FAQs',
+          drawerIcon: () => (
+            <Image source={icons.faqsIcon} style={styles.iconStyle} />
+          ),
+        }}
+        name="FAQs"
+        component={FAQs}
+      />
+      <Drawer.Screen
+        options={{
+          title: 'T&C',
+          drawerIcon: () => (
+            <Image source={icons.tndcIcon} style={styles.iconStyle} />
+          ),
+        }}
+        name="TermsAndConditions"
+        component={TermsAndConditions}
+      />
+      <Drawer.Screen
+        options={{
+          title: 'Support',
+          drawerIcon: () => (
+            <Image source={icons.supportIcon} style={styles.iconStyle} />
+          ),
+        }}
+        name="Support"
+        component={Support}
+      />
     </Drawer.Navigator>
-
   );
 }
-
 
 const styles = StyleSheet.create({
   stack: {
@@ -191,12 +257,11 @@ const styles = StyleSheet.create({
     // overflow: 'scroll',
     // borderWidth: 1,
   },
-  text: { fontSize: Responsive.width(12), fontWeight: '400' },
+  text: { fontSize: Responsive.width(16), color: '#6F7FAF', fontFamily: 'Exo2Bold' },
   drawerStyles: { flex: 1, width: '60%', backgroundColor: 'transparent' },
   header: {
-    backgroundColor: "red",
-    height: Responsive.height(80),
-    width: '100%',
+    // height: Responsive.height(80),
+    // width: '100%',
   },
   userInformation: {
     marginTop: Responsive.height(6),
@@ -205,11 +270,12 @@ const styles = StyleSheet.create({
   },
   item: {
     width: '100%',
-    height: Responsive.height(40),
+    height: Responsive.height(60),
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: Responsive.height(8),
+    // marginTop: Responsive.height(8),
+    // paddingVertical: SIZES.padding3
   },
   leftIcon: {
     width: Responsive.width(42),
@@ -225,7 +291,14 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconStyle: {
+    width: 30,
+    height: 24,
+    resizeMode: 'contain',
+    tintColor: '#6f7faf',
+    marginRight: 18,
   },
 });
